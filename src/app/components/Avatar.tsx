24f93bb5794dfa2c916ec5a1b7db0cdb5485d656
sunilcostacloud <sunil@costacloud.com>
2024-02-19 10:10:03 +0530

    update

---
+++ src/app/components/Avatar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image src='/images/placeholder.jpg' className="rounded-full" height='30' width="30" alt="Avatar" />
+  )
+}
+
+export default Avatar
---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Nunito } from 'next/font/google'
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import ClientOnly from "./components/ClientOnly";
+
+const font = Nunito({
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {/* <ClientOnly> */}
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+        {/* </ClientOnly> */}
+        {children}
+      </body>
+    </html>
+  );
+}
