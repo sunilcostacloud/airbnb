cd95444851c8e88744bdec9c3caf8c647a6c8182
sunilcostacloud <sunil@costacloud.com>
2024-02-21 12:08:42 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Nunito } from 'next/font/google'
+import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import ClientOnly from "./components/ClientOnly";
+import LoginModal from "./components/modals/LoginModal";
 
 const font = Nunito({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={font.className}>
         {/* <ClientOnly> */}
         <ToasterProvider />
+        <LoginModal />
         <RegisterModal />
         <Navbar />
         {/* </ClientOnly> */}
